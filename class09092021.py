4e2d8c196caea08784b01c9136b9239b6fa7ece2
Jason88888xu <89811881+Jason88888xu@users.noreply.github.com>
2021-09-09 20:48:46 -0400

    Class 09092021
    Class 09092021 recitation Rongsheng(Jason) Xu

---
+++ class09092021.py
@@ -0,0 +1,21 @@
+print(2+6)
+print(6-2)
+print(6/2)
+print(6*2)
+print(6%2)
+print(6**2)
+a=5
+b=3
+c="Hello"
+print(c[-3])
+print(c[0])
+print(c[1:3])
+print(c[:3])
+print(c[3:])
+print("type of variable a is",type(a))
+print("type of variable a is",type(b))
+print("type of variable a is",type(c))
+point_obtained=10
+total_point=100
+percentage=(point_obtained/total_point)*100
+print("the percentage is",percentage,"%")
